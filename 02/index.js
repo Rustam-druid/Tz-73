8f7e51c7ca4ed5f672ee4a886700b3c7805e53b6
Mabo Rustam <kimlael01@mail.ru>
2024-11-26 12:42:54 +0600

    second tz done

--- 02/index.js
+++ 02/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const Vigenere = require('caesar-salad').Vigenere;
 
 const app = express();
 const port = 8000;
+const password=1
 
-
-
+app.get('/', (req, res) => {
+    return res.send('Welcom !');
+});
+app.get('/encode/:hello', (req, res) => {
+  const v=  Vigenere.Cipher(password).crypt('abc-0123456789@example.com');
+    return res.send(v);
+});
+app.get('/decode/:d', (req, res) => {
+  const v=  Vigenere.Decipher(password).crypt('bcd-0123456789@fybnqmf.dpn');
+    return res.send(v);
+});
 
 
 app.listen(port, () => {
