4f3a8c927f0aa1fee307750c5c806e8660c0c21e
Mabo Rustam <kimlael01@mail.ru>
2024-11-26 11:24:18 +0600

    first tz done

---
+++ 01/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = 8000;
+
+
+app.get('/', (req, res) => {
+    return res.send('Welcom !');
+});
+app.get('/hello', (req, res) => {
+    return res.send('<h1>Hello </h1>');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
